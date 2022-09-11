3b6d62ce7f8a60752aa2cea0c0a7ff1d569d9388
PunchCakee <email@example.com>
2022-09-11 21:23:17 +0300

    ...

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import dearpygui.dearpygui as imgui
 import openai
 import sqlite3
-
-imgui.create_context()
+import os
 
 """
 TODO: Fine tune the bot
@@ -10,27 +9,40 @@ TODO: sqlite UUID checking
 TODO: obfuscation
 """
 
-def ai():
-    pass
+openai.api_key = ""
 
+imgui.create_context()
 
-def send():
-    
-    is_null = False
+for_export = []
+
+def export():
+    with open('export.txt','w') as file:
+        file.write(str(for_export))
+
+def send():  
     input_box = str(imgui.get_value(item="input_field")).lstrip().rstrip()
-    
-    if not input_box:
-        is_null = True
+    imgui.add_text(f"You: {input_box}", parent="child window")
+    imgui.add_text(f"Alice:\n {ai(input_box)}", parent="child window")
 
-    elif is_null == False:
-        with imgui.child_window(autosize_x=True, label="childwindow", parent="primary window", height=50):
-            imgui.add_text(f"You: {input_box}")
-
-
+def ai(input_box):
+    response = openai.Completion.create(
+      model="text-davinci-002",
+      prompt=input_box,
+      temperature=0.7,
+      max_tokens=4000,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    text = str(response['choices'][0]['text']).strip()
+    for_export.append(text)
+    return text
     
 with imgui.window(tag="primary window", label=None,no_move=True, no_collapse=True, no_title_bar=True):
     imgui.add_input_text(label="type a prompt",tag="input_field", callback=send, on_enter=True)
     imgui.add_button(label="send",callback=send)
+    imgui.add_checkbox(label="check to export prompt", callback=export)
+    imgui.add_child_window(tag="child window",horizontal_scrollbar=True,autosize_x=True,autosize_y=True)
 
 
 #must be in this order for window to stay
